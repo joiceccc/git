ad5f517e0cb0b3e6d85bf2155f544f39da12bc56
Joyce Cheung <joice@Joyces-MacBook-Pro.local>
2016-05-31 16:32:58 +0800

    finished

--- javascript.js
+++ javascript.js
@@ -4,3 +4,11 @@ window.onload = function(){
     prompt('What is your email?')
   });
 }
+
+window.onload = function(){
+  var button = document.getElementById("button-yellow");
+  button.addEventListener("click", function(event){
+    prompt('What is your email?');
+    button.innerHTML = "Thanks for your email!"
+  });
+}
